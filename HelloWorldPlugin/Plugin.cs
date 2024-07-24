e7a892d774eedac95ebeaebcd558c62a26947403
WRC <hello_wrc@outlook.com>
2024-07-24 11:36:40 +0800

    添加项目文件。

---
+++ HelloWorldPlugin/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using ClassIsland.Core.Abstractions;
+using ClassIsland.Core.Attributes;
+using ClassIsland.Core.Controls.CommonDialog;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HelloWorldPlugin;
+
+[PluginEntrance]
+public class Plugin : PluginBase
+{
+    public override void Initialize(HostBuilderContext context, IServiceCollection services)
+    {
+        CommonDialog.ShowInfo("Hello world!");
+    }
+
+    public override void OnShutdown()
+    {
+    }
+}
